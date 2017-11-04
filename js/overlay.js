2847f9760b886e0f7e5a9dd7867051148ad63546
Arthur C Edwards <Arthur.Edwards321@gmail.com>
2017-11-03 21:22:09 -0400

    updated js practice

--- js/overlay.js
+++ js/overlay.js
@@ -7,3 +7,16 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+var name = "man";
+function showMyName(name){
+  return name;
+}
+
+
+
+function bigBootyBitches(buttonId,divId) {
+  document.getElementById(buttonId).addEventListener("click",function(){
+    document.getElementById(divId).setAttribute("style", "display: block");
+  });
+}
+// bigBootyBitches("Beyonce","Naked");
